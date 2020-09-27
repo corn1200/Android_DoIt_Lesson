cb670b576a261e354755aeb7ba3b8858d0b025e3
corn1200 <corn120033@gmail.com>
2020-09-27 23:23:10 +0900

    part02 chapter02 04 RelativeLayout

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -1,10 +1,6 @@
 package com.example.hello;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,19 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void onButton1Clicked(View v) {
-        Toast.makeText(this, "확인1 버튼이 눌렸어요.", Toast.LENGTH_LONG).show();
-    }
-
-    public void onButton2Clicked(View v) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
-        startActivity(intent);
-    }
-
-    public void onButton3Clicked(View v) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-9274-2464"));
-        startActivity(intent);
     }
 }
