62edb6e73e321e97b0997f7c994d2a34ee3fcfa1
corn1200 <corn120033@gmail.com>
2020-10-01 23:03:03 +0900

    part02 chapter03 06 event

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.hello;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        View view = findViewById(R.id.view);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
     }
 
 }
