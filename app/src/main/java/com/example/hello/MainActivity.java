a378df2259bd7640c8c6d62f7eaba167eabb5733
corn1200 <corn120033@gmail.com>
2020-10-02 20:54:48 +0900

    part02 chapter04 05 Positive intent & Negative intent

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.hello;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,23 +18,14 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
-                startActivityForResult(intent, 101);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1000-1000"));
+                startActivity(intent);
+
+//                Intent intent = new Intent();
+//                ComponentName name = new ComponentName("org.example.hello", "org.example.hello.menuActivity");
+//                intent.setComponent(name);
+//                startActivityForResult(intent, 101);
             }
         });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 101) {
-            if (data != null) {
-                String name = data.getStringExtra("name");
-                if (name != null) {
-                    Toast.makeText(this, "응답으로 받은 데이터 : " + name, Toast.LENGTH_LONG).show();
-                }
-            }
-        }
     }
 }
