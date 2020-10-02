3e532dfaf7264ef91021ff988824f257f0b5ac84
corn1200 <corn120033@gmail.com>
2020-10-02 19:00:13 +0900

    part02 chapter04 1 inflation

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -3,32 +3,22 @@ package com.example.hello;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    ProgressBar progressBar;
-
-    int value = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        progressBar = findViewById(R.id.progressBar);
-
-        Button button = findViewById(R.id.button5);
+        Button button = findViewById(R.id.button6);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                value += 10;
-                if (value > 100) {
-                    value = 0;
-                }
 
-                progressBar.setProgress(value);
             }
         });
+
+        setContentView(R.layout.activity_main);
     }
 }
