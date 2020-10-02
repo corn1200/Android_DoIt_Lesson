d53c63178f09a2bd9d59116030b3bb82082e1abf
corn1200 <corn120033@gmail.com>
2020-10-02 10:25:46 +0900

    part02 chapter03 10 screen rotate & status save

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     String name;
     EditText editText;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +23,36 @@ public class MainActivity extends AppCompatActivity {
         showToast("onCreate 호출됨");
 
         editText = findViewById(R.id.editTextTextPersonName);
+        textView2 = findViewById(R.id.textView2);
 
         Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (editText != null) {
+                        name = editText.getText().toString();
+                        showToast("사용자 입력값을 name 변수에 할당함");
+                    }
+                }
+            });
+        }
 
+        if (savedInstanceState != null) {
+            if (textView2 != null) {
+                name = savedInstanceState.getString("name");
+                textView2.setText(name);
+
+                showToast("값을 복원했습니다 : " + name);
             }
-        });
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString("name", name);
     }
 
     @Override
