3b1c537cf10414667c20b2c17d81b5d8b482e09f
corn1200 <corn120033@gmail.com>
2020-10-02 20:29:44 +0900

    part02 chapter04 03 intent

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hello;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,15 +11,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button6);
+        Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivityForResult(intent, 101);
             }
         });
-
-        setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/com/example/hello/MenuActivity.java
+++ app/src/main/java/com/example/hello/MenuActivity.java
@@ -1,38 +1,14 @@
 package com.example.hello;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
-    LinearLayout container;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
-        container = findViewById(R.id.container);
-
-        Button button2 = findViewById(R.id.button);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addLayout();
-            }
-        });
-    }
-
-    private void addLayout() {
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.sub1, container, true);
-
-        Toast.makeText(this, "부분 화면이 추가되었습니다", Toast.LENGTH_LONG).show();
     }
 }
