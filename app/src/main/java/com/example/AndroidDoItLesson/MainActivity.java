920043a53525285099fa05bfa7a9bca348442ec6
corn1200 <corn120033@gmail.com>
2020-10-06 20:57:00 +0900

    part02 chapter05 09 tab

--- app/src/main/java/com/example/AndroidDoItLesson/MainActivity.java
+++ app/src/main/java/com/example/AndroidDoItLesson/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
