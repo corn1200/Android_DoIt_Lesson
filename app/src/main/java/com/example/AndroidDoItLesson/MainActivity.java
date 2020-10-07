f28d5896e4ce59322add572db367c6d315238ebf
corn1200 <corn120033@gmail.com>
2020-10-07 17:45:13 +0900

    part02 chapter05 11 bottom tab

--- app/src/main/java/com/example/AndroidDoItLesson/MainActivity.java
+++ app/src/main/java/com/example/AndroidDoItLesson/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.AndroidDoItLesson;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.appcompat.app.ActionBar;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    Toolbar toolbar;
-
     Fragment1 fragment1;
     Fragment2 fragment2;
     Fragment3 fragment3;
@@ -20,45 +18,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayShowTitleEnabled(false);
-
         fragment1 = new Fragment1();
         fragment2 = new Fragment2();
         fragment3 = new Fragment3();
 
         getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
 
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.addTab(tabs.newTab().setText("통화기록"));
-        tabs.addTab(tabs.newTab().setText("스팸기록"));
-        tabs.addTab(tabs.newTab().setText("연락처"));
-
-        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                int position = tab.getPosition();
-
-                if (position == 0) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
-                } else if (position == 1) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
-                } else if (position == 2) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment3).commit();
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.tab1:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+                        return true;
+                    case R.id.tab2:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+                        return true;
+                    case R.id.tab3:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment3).commit();
+                        return true;
                 }
-            }
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
+                return false;
             }
         });
     }
