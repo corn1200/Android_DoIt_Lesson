922bc9157c03da5fd30c48a1887b17c5d59c1357
corn1200 <corn120033@gmail.com>
2020-10-09 15:45:06 +0900

    part02 chapter06 03 broad cast receiver

---
+++ app/src/main/java/com/example/AndroidDoItLesson/SmsReceiver.java
@@ -0,0 +1,33 @@
+package com.example.AndroidDoItLesson;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+public class SmsReceiver extends BroadcastReceiver {
+    private static final String TAG = "SmsReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive 호출됨");
+
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] message = parseSmsMessage(bundle);
+
+    }
+
+    private SmsMessage[] parseSmsMessage(Bundle bundle) {
+        Object[] objs = (Object[]) bundle.get("pdus");
+        SmsMessage[] messages = new SmsMessage[objs.length];
+
+        int smsCount = objs.length;
+        for (int i = 0; i < smsCount; i++) {
+            SmsMessage.createFromPdu((byte[]) objs[i]);
+        }
+
+        return null;
+    }
+}
