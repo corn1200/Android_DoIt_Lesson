98c19d29e31927d4dc8b9042e0daf4bd5f7cc0f2
corn1200 <corn120033@gmail.com>
2020-10-10 20:19:58 +0900

    part02 chapter07 03 view customize

--- app/src/main/java/com/example/AndroidDoItLesson/MyButton.java
+++ app/src/main/java/com/example/AndroidDoItLesson/MyButton.java
@@ -1,8 +1,11 @@
 package com.example.AndroidDoItLesson;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -28,5 +31,35 @@ public class MyButton extends AppCompatButton {
 
         float textSize = getResources().getDimension(R.dimen.text_size);
         setTextSize(textSize);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Log.d("MyButton", "onDraw 호출됨");
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d("MyButton", "onTouchEvent 호출됨");
+
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                setBackgroundColor(Color.BLUE);
+                setTextColor(Color.RED);
+                break;
+            case MotionEvent.ACTION_OUTSIDE:
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                setBackgroundColor(Color.CYAN);
+                setTextColor(Color.BLACK);
+                break;
+        }
+
+        invalidate();
+
+        return true;
     }
 }
