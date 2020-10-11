ddbb22d8a666b8e6740abd341b0afb259da52d35
corn1200 <corn120033@gmail.com>
2020-10-11 13:23:22 +0900

    part02 chapter07 07 customize layout

--- app/src/main/java/com/example/AndroidDoItLesson/PersonAdapter.java
+++ app/src/main/java/com/example/AndroidDoItLesson/PersonAdapter.java
@@ -1,11 +1,37 @@
 package com.example.AndroidDoItLesson;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class PersonAdapter /*extends RecyclerView.Adapter<PersonAdapter.ViewHolder>*/ {
+import java.util.ArrayList;
+
+public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.ViewHolder> {
+    ArrayList<Person> items = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.person_item, parent, false);
+
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Person item = items.get(position);
+        holder.setItem(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
         TextView textView;
