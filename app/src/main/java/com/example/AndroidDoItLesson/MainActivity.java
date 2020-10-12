b03be600fa58d8a3a1ca370b57c0b985cb2b4bce
corn1200 <corn120033@gmail.com>
2020-10-12 20:59:07 +0900

    part02 chapter08 01 animation

--- app/src/main/java/com/example/AndroidDoItLesson/MainActivity.java
+++ app/src/main/java/com/example/AndroidDoItLesson/MainActivity.java
@@ -2,40 +2,25 @@ package com.example.AndroidDoItLesson;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
-
-    String[] items = {"mike", "angel", "crow", "john", "sean"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
-
-        Spinner spinner = findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                textView.setText(items[position]);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                textView.setText("");
+            public void onClick(View v) {
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale);
+                v.startAnimation(anim);
             }
         });
     }
