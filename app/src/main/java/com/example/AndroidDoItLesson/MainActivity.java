9b15e1f2d542f1faac3913cc8e21781483707922
corn1200 <corn120033@gmail.com>
2020-10-13 08:35:33 +0900

    part02 chapter08 03 animation / translate

--- app/src/main/java/com/example/AndroidDoItLesson/MainActivity.java
+++ app/src/main/java/com/example/AndroidDoItLesson/MainActivity.java
@@ -5,23 +5,60 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Animation translateLeftAnim;
+    Animation translateRightAnim;
+
+    LinearLayout page;
+    Button button;
+
+    boolean isPageOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button2 = findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
+        page = findViewById(R.id.page);
+
+        translateLeftAnim = AnimationUtils.loadAnimation(this, R.anim.translate_left);
+        translateRightAnim = AnimationUtils.loadAnimation(this, R.anim.translate_right);
+
+
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Animation anim2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale2);
-                v.startAnimation(anim2);
+                page.setVisibility(View.VISIBLE);
+                page.startAnimation(translateLeftAnim);
             }
         });
     }
+
+    class SlidingAnimationListener implements Animation.AnimationListener {
+        @Override
+        public void onAnimationStart(Animation animation) {
+            if (isPageOpen) {
+
+            } else {
+
+            }
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    }
+
 }
